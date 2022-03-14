a1430b5a524baabb98c2996bd902eb8118c20f15
Drumond <rrdrumond@gmail.com>
2022-03-14 22:18:09 +0000

    Add images/image5, images/iamge6, images/image8, images/image9, images/logo3, images/principalimage, images/quote2, index.html, main.js, style.css

---
+++ main.js
@@ -0,0 +1,12 @@
+ScrollReveal().reveal('.header-logo', {delay: 200});
+ScrollReveal().reveal('.logo', {delay: 200});
+ScrollReveal().reveal('.button', {delay: 300});
+ScrollReveal().reveal('.hero', {delay: 300});
+ScrollReveal().reveal('.information', {delay: 400});
+ScrollReveal().reveal('.hero-secondary', {delay: 400});
+ScrollReveal().reveal('.quote', {delay: 500});
+ScrollReveal().reveal('.action', {delay: 500});
+ScrollReveal().reveal('.info', {delay: 500});
+ScrollReveal().reveal('.info2', {delay: 600});
+ScrollReveal().reveal('.info3', {delay: 700});
+ScrollReveal().reveal('.info4', {delay: 800});
